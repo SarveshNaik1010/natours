6f44cce8f2577541f6ec9afd8dd4fee7a72c26f3
Sarvesh Naik <naiksarvesh463@gmail.com>
2023-07-15 19:18:39 +0530

    Added a comment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug'); //No need to require
